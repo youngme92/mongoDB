25f2159c49c8796693d7afbfc2617923643d5708
youngme92 <youngme992@gmail.com>
2020-04-02 21:14:54 +0900

    v7

---
+++ v7/routes/comments.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const router  = express.Router({mergeParams: true})
+const hotArtist = require('../models/artist')
+const Comment   = require('../models/comment')
+
+////////////////
+// COMMENT Route
+////////////////
+
+// NEW COMMENT Routing! 
+router.get("/new", isLoggedIn, function(req, res){
+    hotArtist.findById(req.params.id, function(err, foundArtist){
+        if(err){
+            console.log(err)
+        } else {
+            res.render('comments/new', {artist : foundArtist})
+        }
+    })
+})
+
+// CREATE COMMENT Routing
+router.post('/', isLoggedIn, function(req, res){
+    hotArtist.findById(req.params.id, function(err, foundArtist){
+        if(err){
+            console.log(err)
+            res.redirect('/index')
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err)
+                } else {
+                    comment.author.username = req.user.username
+                    comment.author.id = req.user._id
+                    comment.save()
+                    foundArtist.comments.push(comment)
+                    foundArtist.save()
+                    res.redirect('/index/'+ req.params.id)
+                }
+            })
+        }
+    })
+})
+
+// MiddleWare
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    } else {
+        res.redirect('/login')
+    }
+}
+
+module.exports = router
